831bcc44045f5e3f82c5274b70b96907e2c05400
Alex <20131328+Alex-Design@users.noreply.github.com>
2022-07-05 01:12:28 +0100

    Added tailwind and some styling

--- src/App.vue
+++ src/App.vue
@@ -1,126 +1,55 @@
 <link rel="stylesheet" href="assets/base.css">
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import {RouterLink, RouterView} from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <!-- Sidebar -->
+  <div class="sidebar light-grey bar-block" style="width:20%">
+    <h3 class="bar-item">LifeSim</h3>
+    <a href="#" class="bar-item button">Link 1</a>
+    <a href="#" class="bar-item button">Link 2</a>
+    <a href="#" class="bar-item button">Link 3</a>
+  </div>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <!-- Page Content -->
+  <div style="margin-left:20%">
+
+    <div class="container banner">
+      <h1>Stats Here</h1>
     </div>
-  </header>
 
-  <RouterView />
+    <div class="container">
+      <h2>Test Page</h2>
+      <p class="ml-1">Test Text</p>
+    </div>
+
+  </div>
+
+  <!--  <header>-->
+  <!--    <img-->
+  <!--      alt="Vue logo"-->
+  <!--      class="logo"-->
+  <!--      src="@/assets/logo.svg"-->
+  <!--      width="125"-->
+  <!--      height="125"-->
+  <!--    />-->
+
+  <!--    <div class="wrapper">-->
+  <!--      <HelloWorld msg="You did it!" />-->
+
+  <!--      <nav>-->
+  <!--        <RouterLink to="/">Home</RouterLink>-->
+  <!--        <RouterLink to="/about">About</RouterLink>-->
+  <!--      </nav>-->
+  <!--    </div>-->
+  <!--  </header>-->
+
+  <!--  <RouterView />-->
 </template>
 
 <style>
 @import "@/assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
